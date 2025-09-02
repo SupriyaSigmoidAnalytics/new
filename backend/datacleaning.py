a833401898205fe29a31efb920a0a4ec591f9c6a
SupriyaBC <supriyachougale2626@gmail.com>
2025-09-02 16:57:09 +0530

    Handled missing values of date and discount column

--- backend/datacleaning.py
+++ backend/datacleaning.py
@@ -5,12 +5,14 @@ data= pd.read_csv(dataset_path)
 
 data['Date']=pd.to_datetime(data['Date'],errors='coerce')
 data['Customer_Name'].fillna('Unkown', inplace=True)
+data['Discount'].fillna(0, inplace=True)
 data['Discount']=data['Discount'].str.replace('%','',regex=True)
 data['Discount'] = pd.to_numeric(data['Discount'], errors='coerce')  
 data['Discount'] = data['Discount'] / 100 
-
+data = data.dropna(subset=['Date'])
 data['Quantity']=data['Quantity'].astype(int)
 data['Price']=data['Price'].astype(float)
+
 
 
 data['sales_before_discount']=data['Price']*data['Quantity']
@@ -20,7 +22,5 @@ sales_comp=data.groupby('Product')[['sales_before_discount','sales_after_discoun
 
 data.to_csv('cleaned_dataset.csv',index=False)
 sales_comp.to_csv('sales_comp.csv',index=False)
-
-
 
 
