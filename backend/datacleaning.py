290a0ee8c6204bf33dea3b3c74df42e99f68829b
SupriyaBC <supriyachougale2626@gmail.com>
2025-09-04 14:02:08 +0530

    rounded values and handled data types and missing values

--- backend/datacleaning.py
+++ backend/datacleaning.py
@@ -5,18 +5,18 @@ data= pd.read_csv(dataset_path)
 
 data['Date']=pd.to_datetime(data['Date'],errors='coerce')
 data['Customer_Name'].fillna('Unkown', inplace=True)
-data['Discount'].fillna(0, inplace=True)
+
 data['Discount']=data['Discount'].str.replace('%','',regex=True)
 data['Discount'] = pd.to_numeric(data['Discount'], errors='coerce')  
 data['Discount'] = data['Discount'] / 100 
+data['Discount'].fillna(0, inplace=True)
+
 data = data.dropna(subset=['Date'])
 data['Quantity']=data['Quantity'].astype(int)
 data['Price']=data['Price'].astype(float)
 
-
-
-data['sales_before_discount']=data['Price']*data['Quantity']
-data['sales_after_discount']=data['Price']*(1-data['Discount'])*data['Quantity']
+data['sales_before_discount']=(data['Price']*data['Quantity']).round(2)
+data['sales_after_discount']=(data['Price']*(1-data['Discount'])*data['Quantity']).round(2)
 
 sales_comp=data.groupby('Product')[['sales_before_discount','sales_after_discount']].sum().reset_index()
 
